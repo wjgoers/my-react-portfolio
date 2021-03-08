b72777585d2afbee2981ef0d562551a5c2de457c
Bill Goers <wjgoers@gmail.com>
2021-03-08 13:45:16 -0600

    css and home page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Contact from './components/Contact'
 import Portfolio from './components/Portfolio'
+import Homepage from './components/Homepage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello
         <Portfolio />
         <Contact />
+        <Homepage />
   
       </header>
     </div>
---
+++ src/components/Homepage.js
@@ -0,0 +1,9 @@
+function Homepage() {
+    return(
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Homepage;
