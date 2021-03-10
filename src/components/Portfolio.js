3a03d23dbdf86f9b302b51a32481af0d14cb4247
Bill Goers <wjgoers@gmail.com>
2021-03-10 15:31:56 -0600

    components added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,23 @@
 function Portfolio(){
     return(
-        <div></div>
+        <main class="container">
+            <section class="row">
+                <portfolio class="col sm-6">
+                    <row1 class="row">
+                        <div class="col sm-6">
+                            <a href="https://morning-thicket-05206.herokuapp.com/">
+                                <img src="public/Party_Hub.png" width="150" height="150" alt="Placeholder" class="img-thumbnail"></img>
+                            </a>
+                            <br></br>
+                            <a href="https://nwgroup1.github.io/Astro-Mixologist/">
+                                <img src="public/astro-mix.png" width="150" height="150" alt="Placeholder" class="img-thumbnail"></img>
+                            </a>
+                        </div>
+                    </row1>
+                </portfolio>
+
+            </section>
+        </main>
     )
 }
 export default Portfolio;
