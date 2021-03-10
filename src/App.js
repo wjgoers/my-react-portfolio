a133be9ba19b37661a3eb8c267a00c8dee5472af
Bill Goers <wjgoers@gmail.com>
2021-03-10 15:37:39 -0600

    about me component added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Portfolio from './components/Portfolio'
 import Homepage from './components/Homepage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <AboutMe />
         <Portfolio />
         <Contact />
         <Homepage />
         <Footer />
+        
   
       </header>
     </div>
---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+function AboutMe(){
+    return(
+        <main class="container">
+        <section class="row mt-5">
+            <article class="col-sm-12 col-md-8 col-lg-6 card card-body">
+                     <h5>What's Up?</h5>
+                    <p class="copy">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore rem, magni quaerat autem amet id nemo nihil minima doloribus commodi quam repellendus tenetur aliquid mollitia, quisquam dignissimos itaque. Magnam, expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, soluta explicabo. Vitae dignissimos assumenda pariatur! Aliquam id sit repellendus, est tempore nemo deserunt tempora quis consectetur explicabo enim amet. Perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit totam reprehenderit facere amet soluta, quae in dolorum ratione nobis velit? Rem natus architecto nobis eum. Autem laborum quod placeat hic?</p>
+            </article>
+        </section>
+    </main>
+    )
+}
+
+export default AboutMe;
